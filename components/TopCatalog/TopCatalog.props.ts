42d571a6f31bd1043a21463a178a77a8f9d91b7a
Alina <alina2345nakataclovo@gmail.com>
2026-01-23 19:52:11 +0800

    2-catalog: creating component TopCatalog

---
+++ components/TopCatalog/TopCatalog.props.ts
@@ -0,0 +1,7 @@
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+export interface TopCatalogProps
+  extends DetailedHTMLProps<
+    HTMLAttributes<HTMLParagraphElement>,
+    HTMLParagraphElement
+  > {}
